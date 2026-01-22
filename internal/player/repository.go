c9d118c4ab94027654cd4569f534b9eb0a4d98c7
akaci <kara@email.ak>
2026-01-22 13:45:31 +0100

    (forgot to commit player repo interface) changed id with type aliases to be more explicit with parameters. Jwt has also playerId in its claims because it is needed in almost any use case

--- internal/player/repository.go
+++ internal/player/repository.go
@@ -1,9 +1,13 @@
 package player
 
-import "context"
+import (
+	"beldur/internal/id"
+	"context"
+)
 
 type Repository interface {
-	Save(ctx context.Context, player *Player, accountId int) (*Player, error)
+	Save(ctx context.Context, player *Player, accountId id.AccountId) (*Player, error)
 	FindByUsername(ctx context.Context, username string) (*Player, error)
-	FindById(ctx context.Context, playerId int) (*Player, error)
+	FindById(ctx context.Context, playerId id.PlayerId) (*Player, error)
+	FindByAccountId(ctx context.Context, accountId id.AccountId) (*Player, error)
 }
