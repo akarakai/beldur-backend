6f90fedcd75e95cd5f641e77b7bf499370c3043e
akaci <kara@email.ak>
2026-01-26 16:13:43 +0100

    integration test create account endpoint

--- internal/app/account_test.go
+++ internal/app/account_test.go
@@ -3,15 +3,26 @@
 package app
 
 import (
+	"beldur/internal/account"
+	"net/http"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMain(m *testing.M) {
-
-}
-
-func TestFiberApp_Login(t *testing.T) {
-	assert.NotPanics(t, func() {})
+func TestCreateAccount_Success(t *testing.T) {
+	email := "spatagarru@gmail.com"
+	req := account.CreateAccountRequest{
+		Username: "username123",
+		Password: "password123",
+		Email:    &email,
+	}
+	client := &http.Client{
+		Timeout: time.Second,
+	}
+	resp, body := DoJSONOK[account.CreateAccountResponse](t, client, "POST", "/auth/signup", req)
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+	assert.Equal(t, *body.Email, email)
+	assert.Equal(t, body.AccountName, req.Username)
 }
