968889d8cc002652c7e4a6f01c00c0db72297ef4
akaci <kara@email.ak>
2026-01-26 16:11:21 +0100

    app offers now two functions for test and dev env

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"beldur/internal/app"
+	"beldur/pkg/auth/jwt"
+	"beldur/pkg/db/postgres"
+	"beldur/pkg/db/tx"
 	"beldur/pkg/logger"
+	"context"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -17,8 +22,37 @@ func main() {
 
 	port := os.Getenv("PORT")
 
-	fiber := app.New()
+	jwtService := buildJwtService()
+	transactor, querier := buildTransactorQuerierProvider()
+	deps := app.Deps{
+		JwtService: jwtService,
+		Transactor: transactor,
+		QProvider:  querier,
+	}
+
+	fiber := app.NewDev(deps)
 	if err := fiber.Listen(port); err != nil {
 		panic(err)
 	}
+}
+
+func buildJwtService() *jwt.Service {
+	secret := []byte(os.Getenv("JWT_SECRET"))
+	expiration, _ := time.ParseDuration(os.Getenv("JWT_EXPIRATION"))
+	issuer := os.Getenv("JWT_ISSUER")
+	return jwt.NewService(secret, expiration, issuer)
+}
+
+func buildTransactorQuerierProvider() (tx.Transactor, postgres.QuerierProvider) {
+	cfg, err := postgres.ConfigFromEnv()
+	if err != nil {
+		panic(err)
+	}
+
+	pgxPool, err := postgres.NewPgxPool(context.Background(), cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	return postgres.NewTransactor(pgxPool)
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -7,40 +7,59 @@ import (
 	"beldur/pkg/db/postgres"
 	"beldur/pkg/db/tx"
 	"beldur/pkg/middleware"
-	"context"
 	"fmt"
-	"os"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/healthcheck"
 	fiberlogger "github.com/gofiber/fiber/v2/middleware/logger"
 )
 
+type Deps struct {
+	JwtService *jwt.Service
+	Transactor tx.Transactor
+	QProvider  postgres.QuerierProvider
+}
+
 type FiberApp struct {
 	app *fiber.App
 }
 
-func New() *FiberApp {
-	jwtService := buildJwtService()
-	transactor, qProvider := buildTransactorQuerierProvider()
-	accountHandler := account.NewHandlerFromDeps(account.Deps{
-		Transactor: transactor,
-		QProvider:  qProvider,
-		Issuer:     jwtService,
-	})
+func NewDev(deps Deps) *FiberApp {
+	return build(deps, false)
+}
 
-	campaignHandler := campaign.NewHandlerFromDeps(campaign.Deps{
-		QProvider:  qProvider,
-		Transactor: transactor,
-	})
+func NewTest(deps Deps) *FiberApp {
+	return build(deps, true)
+}
 
-	app := fiber.New()
+func build(deps Deps, test bool) *FiberApp {
+	cfg := fiber.Config{}
+	if test {
+		cfg.DisableStartupMessage = true
+	}
+
+	app := fiber.New(cfg)
+
 	app.Use(healthcheck.New())
-	app.Use(fiberlogger.New())
 
-	authMiddleware := middleware.Auth(jwtService)
+	if !test {
+		app.Use(fiberlogger.New())
+	}
 
+	// handlers
+	accountHandler := account.NewHandlerFromDeps(account.Deps{
+		Transactor: deps.Transactor,
+		QProvider:  deps.QProvider,
+		Issuer:     deps.JwtService,
+	})
+	campaignHandler := campaign.NewHandlerFromDeps(campaign.Deps{
+		QProvider:  deps.QProvider,
+		Transactor: deps.Transactor,
+	})
+
+	authMiddleware := middleware.Auth(deps.JwtService)
+
+	// routes
 	app.Post("/auth/signup", middleware.Validation[account.CreateAccountRequest](), accountHandler.Register)
 	app.Post("/auth/login", middleware.Validation[account.UsernamePasswordLoginRequest](), accountHandler.Login)
 	app.Post("/campaign", authMiddleware, middleware.Validation[campaign.CreationRequest](), campaignHandler.HandleCreateCampaign)
@@ -48,32 +67,9 @@ func New() *FiberApp {
 	app.Patch("/account", authMiddleware, accountHandler.UpdateAccount)
 	app.Post("/campaign/:campaignId", authMiddleware, middleware.Validation[campaign.JoinRequest](), campaignHandler.HandleJoinCampaign)
 
-	return &FiberApp{
-		app: app,
-	}
+	return &FiberApp{app: app}
 }
 
 func (app *FiberApp) Listen(port string) error {
 	return app.app.Listen(fmt.Sprintf(":%s", port))
-}
-
-func buildJwtService() *jwt.Service {
-	secret := []byte(os.Getenv("JWT_SECRET"))
-	expiration, _ := time.ParseDuration(os.Getenv("JWT_EXPIRATION"))
-	issuer := os.Getenv("JWT_ISSUER")
-	return jwt.NewService(secret, expiration, issuer)
-}
-
-func buildTransactorQuerierProvider() (tx.Transactor, postgres.QuerierProvider) {
-	cfg, err := postgres.ConfigFromEnv()
-	if err != nil {
-		panic(err)
-	}
-
-	pgxPool, err := postgres.NewPgxPool(context.Background(), cfg)
-	if err != nil {
-		panic(err)
-	}
-
-	return postgres.NewTransactor(pgxPool)
 }
